2679bd2cdfe257763063aaf2675b01f007954627
meGAmeS1 <florentin.lemoal@gmail.com>
2015-11-25 00:07:06 +0100

    Fixing for Django 1.8 compatibility

--- sample/urls.py
+++ sample/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('',
-    url(r'.*', 'sample.todo_app.views.edit'),
-)
+from django.conf.urls import url
 
 from django.conf.urls.static import static
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns = [
+    url(r'.*', 'sample.todo_app.views.edit'),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+]
